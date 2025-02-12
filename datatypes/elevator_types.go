9b66d5defde46cf667d65587dfb196852f9c1068
monarogg <157384787+monarogg@users.noreply.github.com>
2025-02-12 10:48:22 +0100

    begynte på request_assigner, og gjorde små endringer på datatypes

--- datatypes/elevator_types.go
+++ datatypes/elevator_types.go
@@ -2,30 +2,31 @@ package datatypes
 
 import (
 	"project/elevio"
-	"time"
 )
 
 const N_FLOORS = 4
 const N_BUTTONS = 3
 const N_HALL_BUTTONS = 2
 
-type ElevatorState int
+type ElevBehaviour int
 
 const (
-	Idle     ElevatorState = 0
-	Moving   ElevatorState = 1
-	DoorOpen ElevatorState = 2
+	Idle     ElevBehaviour = 0
+	Moving   ElevBehaviour = 1
+	DoorOpen ElevBehaviour = 2
 )
 
 type Elevator struct {
 	CurrentFloor int
 	Direction    elevio.MotorDirection
-	State        ElevatorState
-	Orders       [4][3]bool
-	Config       ElevatorConfig
-	StopActive   bool
+	State        ElevBehaviour
+	Orders       [N_FLOORS][N_BUTTONS]bool
 }
 
-type ElevatorConfig struct {
-	DoorOpenDuration time.Duration
+type ElevSharedInfo struct {
+	Available    bool
+	Behaviour    ElevBehaviour
+	Direction    elevio.MotorDirection
+	CurrentFloor int
+	// SKAL VI HA EN MUTEX HER?
 }
--- datatypes/request_types.go
+++ datatypes/request_types.go
@@ -1,3 +1,34 @@
 package datatypes
 
-type RequestType int
+import "project/elevio"
+
+type RequestState int
+
+const (
+	Completed  RequestState = 0
+	Unassignes RequestState = 1
+	Assigned   RequestState = 2
+)
+
+type RequestType struct {
+	State     RequestState
+	Count     int
+	AwareList []string
+}
+
+type ElevatorInfo struct {
+	Available bool
+	Behaviour ElevBehaviour
+	Direction elevio.MotorDirection
+	Floor     int
+}
+
+type NetworkMsg struct {
+	SenderID           string
+	Available          bool
+	Behavior           ElevBehaviour
+	Direction          elevio.MotorDirection
+	Floor              int
+	SenderHallRequests [N_FLOORS][N_BUTTONS]RequestType
+	allCabRequests     map[string][N_FLOORS]RequestType
+}
