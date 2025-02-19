78da357ed88db6d30f1b06fd8a64e47b306dd899
Adrian Haga <100347486+HA-GA@users.noreply.github.com>
2025-02-19 08:50:28 +0100

    Update elevator_types.go
    la tile netelevator for broadcast

--- datatypes/elevator_types.go
+++ datatypes/elevator_types.go
@@ -27,6 +27,16 @@ type Elevator struct {
 	StopActive   bool
 }
 
+type NetElevator struct {
+	ID           string
+	CurrentFloor int
+	Direction    elevio.MotorDirection
+	State        ElevatorState
+	Orders       [4][3]bool
+	Config       ElevatorConfig
+	StopActive   bool
+}
+
 type ElevSharedInfo struct {
 	Available    bool
 	Behaviour    ElevBehaviour
