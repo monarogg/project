cdd963ce0663ffebc9f8d14b8e136010dac013d5
monarogg <157384787+monarogg@users.noreply.github.com>
2025-03-11 13:00:08 +0100

    ikke store endringer

--- elevator_control/elevator_manager.go
+++ elevator_control/elevator_manager.go
@@ -6,82 +6,6 @@ import (
 	"time"
 )
 
-// var sharedInfo datatypes.ElevSharedInfo
-
-// type ElevatorManager struct {
-// 	elevators map[string]*datatypes.Elevator
-// 	contexts  map[string]*datatypes.ElevatorContext
-// 	mutex     sync.Mutex
-// }
-
-// // oppretter global instans av ElevatorManager
-// var manager = ElevatorManager{
-// 	elevators: make(map[string]*datatypes.Elevator),
-// 	contexts:  make(map[string]*datatypes.ElevatorContext),
-// }
-
-// // Henter / oppretter en elevator for en spesifikk heis:
-// func GetElevator(elevatorID string) *datatypes.Elevator {
-// 	manager.mutex.Lock()
-// 	defer manager.mutex.Unlock()
-
-// 	if elev, exists := manager.elevators[elevatorID]; exists {
-// 		return elev
-// 	}
-
-// 	// hvis heisen ikke finnes: opprett en:
-// 	newElevator := &datatypes.Elevator{
-// 		CurrentFloor: 0,
-// 		State:        datatypes.Idle,
-// 		Orders:       [datatypes.N_FLOORS][datatypes.N_BUTTONS]bool{},
-// 		StopActive:   false,
-// 		Config:       datatypes.ElevatorConfig{},
-// 	}
-
-// 	manager.elevators[elevatorID] = newElevator
-// 	return newElevator
-// }
-
-// // Henter / oppretter en ElevatorContext for en spesifikk heis:
-
-// func GetElevatorContext(elevatorID string) *datatypes.ElevatorContext {
-// 	manager.mutex.Lock()
-// 	defer manager.mutex.Unlock()
-
-// 	if ctx, exists := manager.contexts[elevatorID]; exists {
-// 		return ctx
-// 	}
-
-// 	// hvis contexten ikke finnes, opprett ny:
-// 	newCtx := &datatypes.ElevatorContext{
-// 		AllCabRequests:   make(map[string][datatypes.N_FLOORS]datatypes.RequestType),
-// 		UpdatedInfoElevs: make(map[string]datatypes.ElevatorInfo),
-// 		LocalID:          elevatorID,
-// 	}
-
-// 	manager.contexts[elevatorID] = newCtx
-// 	return newCtx
-// }
-
-// func updateInfoElev(e datatypes.Elevator) {
-// 	sharedInfo.Mutex.RLock() // Read lock, when gorutines only read data, no writing
-// 	defer sharedInfo.Mutex.RUnlock()
-
-// 	sharedInfo.Behaviour = e.State
-// 	sharedInfo.Direction = e.Direction
-// 	sharedInfo.CurrentFloor = e.CurrentFloor
-
-// }
-
-// func setElevAvailability(val bool) {
-// 	sharedInfo.Mutex.Lock() // gorutine skal ha eksklusiv tilgang
-// 	defer sharedInfo.Mutex.Unlock()
-
-// 	sharedInfo.Available = val
-// }
-
-//// NY FRA HER:
-
 var sharedInfoElevs datatypes.ElevSharedInfo
 
 // henter informasjon om heis, returnerer en kopi av heisens tilstand
@@ -98,13 +22,13 @@ func GetInfoElev() datatypes.ElevatorInfo {
 }
 
 // oppdaterer info om heis
-func updateInfoElev(e datatypes.Elevator) {
+func updateInfoElev(elevator datatypes.Elevator) {
 	sharedInfoElevs.Mutex.Lock()
 	defer sharedInfoElevs.Mutex.Unlock()
 
-	sharedInfoElevs.Behaviour = e.State
-	sharedInfoElevs.Direction = e.Direction
-	sharedInfoElevs.CurrentFloor = e.CurrentFloor
+	sharedInfoElevs.Behaviour = elevator.State
+	sharedInfoElevs.Direction = elevator.Direction
+	sharedInfoElevs.CurrentFloor = elevator.CurrentFloor
 }
 
 // endrer tilgjengelighet til heisen basert på val
@@ -131,7 +55,7 @@ func initElevator(chanFloorSensor <-chan int) datatypes.Elevator {
 	elevio.SetMotorDirection(elevio.MD_Stop) // stopper heisen i den funnede etasjen
 	elevio.SetFloorIndicator(currentFloor)   // oppdaterer heisens etasje med lampe
 
-	return datatypes.Elevator{CurrentFloor: currentFloor, Direction: dirConv(datatypes.DIR_STOP), Orders: [datatypes.N_FLOORS][datatypes.N_BUTTONS]bool{}, State: datatypes.Idle}
+	return datatypes.Elevator{CurrentFloor: currentFloor, Direction: datatypes.DIR_STOP, Orders: [datatypes.N_FLOORS][datatypes.N_BUTTONS]bool{}, State: datatypes.Idle}
 }
 
 // starter/nullstiller en timer til et nytt antall sekunder
