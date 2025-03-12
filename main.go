25c291a064bd0e64475cb3395a447c7eff75ad91
Adrian Haga <100347486+HA-GA@users.noreply.github.com>
2025-03-12 09:58:18 +0100

    Update main.go

--- main.go
+++ main.go
@@ -3,20 +3,16 @@ package main
 import (
 	"flag"
 	"fmt"
-	"project/datatypes"
 	"project/elevator_control"
+	"project/elevator_network"
 	"project/elevio"
-	"project/network/bcast"
-	"project/network/peers"
-	"time"
 )
 
 func main() {
-
 	idFlag := flag.String("id", "ElevDefault", "Unique ID for this elevator")
-	portFlag := flag.String("port", "15657", "Simulator port") // Define both flags first
+	portFlag := flag.String("port", "15657", "Simulator port")
 
-	flag.Parse() // Call flag.Parse() only once
+	flag.Parse()
 
 	myID := *idFlag
 	port := *portFlag
@@ -24,54 +20,21 @@ func main() {
 	numFloors := 4
 	elevio.Init("localhost:"+port, numFloors)
 
-	//channels for peers
-	txPeerEnable := make(chan bool)
-	rxPeerUpdates := make(chan peers.PeerUpdate)
+	// Initialize elevator network
+	elevatorNetwork := elevator_network.InitElevatorNetwork(myID)
 
-	// start the transmitter/receiver for peers
-	go bcast.Transmitter(17658, txPeerEnable)
-	go bcast.Receiver(17658, rxPeerUpdates)
-
-	//Start broadcasting ID
-	txPeerEnable <- true
-
-	//channels for broadcasting elevator state
-	txElevatorState := make(chan datatypes.NetElevator)
-	rxElevatorState := make(chan datatypes.NetElevator)
-
-	// start the transmitter/receiver for states
-	go bcast.Transmitter(17657, txElevatorState)
-	go bcast.Receiver(17657, rxElevatorState)
-
+	// Initialize elevator
 	elevator := elevator_control.InitializeFSM()
-	knownElevators := make(map[string]datatypes.NetElevator)
 	context := elevator_control.GetElevatorContext(myID)
 
-	//Heratbeat broadcasting state,
-	go func() {
+	// Start broadcasting heartbeat
+	elevatorNetwork.StartHeartbeat(&elevator)
 
-		for {
-			currentState := datatypes.NetElevator{
-				ID:           myID,
-				CurrentFloor: elevator.CurrentFloor,
-				Direction:    elevator.Direction,
-				State:        elevator.State,
-				Orders:       elevator.Orders,
-				StopActive:   elevator.StopActive,
-			}
-			txElevatorState <- currentState
-			time.Sleep(100 * time.Millisecond)
-		}
-	}()
+	// Listen for elevator states
+	elevatorNetwork.ListenForStates()
 
-	//Listen for states
-	go func() {
-		for {
-			tempState := <-rxElevatorState
-			knownElevators[tempState.ID] = tempState
-			fmt.Println("Received state from:", tempState.ID, "Floor:", tempState.CurrentFloor)
-		}
-	}()
+	// Start detecting missing elevators
+	go elevatorNetwork.DetectMissingElevators()
 
 	drv_buttons := make(chan elevio.ButtonEvent)
 	drv_floors := make(chan int)
@@ -86,24 +49,21 @@ func main() {
 
 	for {
 		select {
-		case a := <-drv_buttons: // a tilsvarer knappetrykket
-			// håndtere trykk på knapper
+		case a := <-drv_buttons:
 			elevator_control.OnRequestButtonPress(&elevator, a.Floor, a.Button, context)
 
-		case a := <-drv_floors: // a blir etasjen man ankommer
+		case a := <-drv_floors:
 			elevator_control.OnFloorArrival(&elevator, a, context)
 
-		case a := <-drv_obstr: // håndterer dersom obstruction blir aktivert
+		case a := <-drv_obstr:
 			if a {
-
 				elevio.SetMotorDirection(elevio.MD_Stop)
 			} else {
 				elevio.SetMotorDirection(elevator.Direction)
 			}
 
-		case <-drv_stop: // håndterer dersom stop blir trykket
+		case <-drv_stop:
 			elevator_control.OnStopButtonPress(&elevator)
-
 		}
 
 		elevator_control.UpdateLights(&elevator)
