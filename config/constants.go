6421401e36f2637a253d3ac593c818d83cd5a165
monarogg <157384787+monarogg@users.noreply.github.com>
2025-03-27 10:04:46 +0100

    ingen

--- config/constants.go
+++ config/constants.go
@@ -49,11 +49,3 @@ const (
 	MD_DOWN MotorDirection = -1
 	MD_STOP MotorDirection = 0
 )
-
-// type ButtonType int
-
-// const (
-// 	BT_HallUP   ButtonType = 0
-// 	BT_HallDOWN ButtonType = 1
-// 	BT_CAB      ButtonType = 2
-// )
